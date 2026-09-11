2a453d1ed64b5364f0e2b1800203749bce7d4444
cv9tou <zopex7878@gmail.com>
2026-09-11 14:35:09 +0300

    first commit

---
+++ src/main/java/ua/edu/duan/test_practic/TestPracticApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.test_practic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestPracticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestPracticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/duan/test_practic/TestPracticApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.duan.test_practic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestPracticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
