c12eb782391d6a7693751c4916db29e271e031cb
Anna-B-2004 <anna.bondarenko.4a@gmail.com>
2026-09-25 15:26:31 +0300

    ParameterizedTest

--- src/main/java/ua/edu/duan/test_practic/CalculatorService.java
+++ src/main/java/ua/edu/duan/test_practic/CalculatorService.java
@@ -4,4 +4,16 @@ public class CalculatorService {
     public int add (int a, int b){
         return a+b ;
     }
+
+    public int cancel (int a, int b){
+        return a-b ;
+    }
+
+    public int divide (int a, int b){
+        return a/b ;
+    }
+
+    public int multiply (int a, int b){
+        return a*b ;
+    }
 }
--- src/test/java/ua/edu/duan/test_practic/CalculatorServiceTest.java
+++ src/test/java/ua/edu/duan/test_practic/CalculatorServiceTest.java
@@ -3,6 +3,9 @@ package ua.edu.duan.test_practic;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -15,4 +18,30 @@ public class CalculatorServiceTest {
         assertEquals(8,result);
 
     }
+
+    @Test
+    void testSubtract(){
+        CalculatorService culculatorservise = new CalculatorService();
+        int result = culculatorservise.cancel(5,3);
+        assertEquals(2,result);
+    }
+
+    @Test
+    void testMultiply(){
+        CalculatorService culculatorservise = new CalculatorService();
+        int result = culculatorservise.multiply(5,3);
+        assertEquals(15,result);
+    }
+
+    @Test
+    void testDivide(){
+        CalculatorService culculatorservise = new CalculatorService();
+        int result = culculatorservise.divide(15,3);
+        assertEquals(5,result);
+    }
+
+    @ParameterizedTest(name = "")
+    @CsvSource
+
 }
+
