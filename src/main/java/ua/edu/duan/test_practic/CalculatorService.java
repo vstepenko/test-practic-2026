edfb6a824141b4ffefb331b58312ef4defb45407
Nasty20-ui <snastya200205@gmail.com>
2026-09-25 14:27:57 +0300

    first test is added

--- src/main/java/ua/edu/duan/test_practic/CalculatorService.java
+++ src/main/java/ua/edu/duan/test_practic/CalculatorService.java
@@ -1,4 +1,7 @@
 package ua.edu.duan.test_practic;
 
 public class CalculatorService {
+    public int add (int a, int b){
+        return a+b ;
+    }
 }
--- src/test/java/ua/edu/duan/test_practic/CalculatorServiceTest.java
+++ src/test/java/ua/edu/duan/test_practic/CalculatorServiceTest.java
@@ -1,4 +1,18 @@
 package ua.edu.duan.test_practic;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class CalculatorServiceTest {
+
+    @Test
+    void testSum(){
+        CalculatorService culculatorservise = new CalculatorService();
+        int result = culculatorservise.add(3,5);
+        assertEquals(8,result);
+
+    }
 }
